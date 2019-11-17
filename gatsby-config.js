61d2f1f40cd264d085e205563034f725ec34c7f6
Josh Buchea <verctrl@joshbuchea.com>
2019-11-17 11:13:12 -0800

    Add a proper site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'JB',
-    description: 'Josh Buchea',
+    description: "Josh Buchea's digital garden",
     siteUrl: 'https://joshbuchea.com'
   },
   plugins: [
